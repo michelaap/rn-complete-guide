8899acbab136cc42c05f0398d508e1c9ba340041
Michel Pereira <michel.augusto@pg.inatel.br>
2019-09-07 08:38:18 -0300

    add alert

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,8 @@ import {
   StyleSheet,
   Button,
   TouchableWithoutFeedback,
-  Keyboard
+  Keyboard,
+  Alert
 } from 'react-native';
 
 import Card from '../components/Card';
@@ -30,7 +31,12 @@ const StartGameScreen = props => {
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
 
-    if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99) {
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        'Invalid number!',
+        'Number has to be a number between 1 and 99.',
+        [{ text: 'Ok', style: 'destructive', onPress: resetInputHandler }]
+      );
       return;
     }
 
