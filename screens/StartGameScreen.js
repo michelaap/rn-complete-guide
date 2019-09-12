21a296657f133591a0829a08d46dc7e5e497fbf7
Michel Pereira <michel.augusto@pg.inatel.br>
2019-09-11 21:00:57 -0300

    working with dimensions

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -6,7 +6,8 @@ import {
   Button,
   TouchableWithoutFeedback,
   Keyboard,
-  Alert
+  Alert,
+  Dimensions
 } from 'react-native';
 
 import Card from '../components/Card';
@@ -111,8 +112,9 @@ const styles = StyleSheet.create({
     fontFamily: 'open-sans-bold'
   },
   inputContainer: {
-    width: 300,
-    maxWidth: '80%',
+    width: '80%',
+    maxWidth: '95%',
+    minWidth: 300 ,
     alignItems: 'center',
   },
   buttonContainer: {
@@ -122,7 +124,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15
   },
   button: {
-    width: 100
+    // width: 100
+    width: Dimensions.get('window').width / 4
   },
   input: {
     width: 50,
