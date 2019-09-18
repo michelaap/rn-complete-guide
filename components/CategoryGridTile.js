3d2ecc1babf2f131a929d129d569a01f55b55bcb
Michel Pereira <michel.augusto@pg.inatel.br>
2019-09-17 21:52:51 -0300

    add new model meal

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -47,7 +47,7 @@ const styles = StyleSheet.create({
     alignItems: 'flex-end'
   },
   title: {
-    fontFamily: 'open-sans-bold',
+    // fontFamily: 'open-sans-bold',
     fontSize: 22,
     textAlign: 'right'
   }
---
+++ models/meal.js
@@ -0,0 +1,30 @@
+class Meal {
+  constructor(id,
+    categoryIds,
+    title,
+    affordability,
+    complexity,
+    imageUrl,
+    duration,
+    ingridients,
+    steps,
+    isGlutenFree,
+    isVegan,
+    isVegetarian,
+    isLactoseFree) {
+      this.categoryIds = categoryIds,
+      this.title = title,
+      this.affordability = affordability,
+      this.complexity = complexity,
+      this.imageUrl = imageUrl,
+      this.duration = duration,
+      this.ingridients = ingridients,
+      this.steps = steps,
+      this.isGlutenFree = isGlutenFree,
+      this.isVegan = isVegan,
+      this.isVegetarian = isVegetarian,
+      this.isLactoseFree = isLactoseFree
+    }
+}
+
+export default Meal;
