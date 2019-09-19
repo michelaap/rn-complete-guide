d41eeba3792c42bd9eccd0b3178862ccd8aecacd
Michel Pereira <michel.augusto@pg.inatel.br>
2019-09-19 20:40:42 -0300

    add navigation tab

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -19,7 +19,9 @@ const CategoryGridTile = props => {
     <View style={styles.gridItem}>
       <TouchableComponent style={{ flex: 1 }} onPress={props.onSelect}>
         <View style={{ ...styles.container, ...{backgroundColor: props.color} }}>
-          <Text style={styles.title} numberOfLines={2}>{props.title}</Text>
+          <Text style={styles.title} numberOfLines={2}>
+            {props.title}
+          </Text>
         </View>
       </TouchableComponent>
     </View>
@@ -32,7 +34,11 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: 'hidden'
+    overflow: 
+      Platform.OS === 'android' && Platform.Version >= 21 
+        ? 'hidden'
+        : 'visible',
+    elevation: 5,
   },
   container: {
     flex: 1,
@@ -41,7 +47,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.26,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 10,
-    elevation: 3,
     padding: 15,
     justifyContent: 'flex-end',
     alignItems: 'flex-end'
