10c758d81af1b41aac57922a864bd07ff864b0a9
Michel Pereira <michel.augusto@pg.inatel.br>
2019-09-20 20:59:07 -0300

    add style to drawer

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -81,11 +81,28 @@ const MealsFavTabNavigator =
 
 const FiltersNavigator = createStackNavigator({
   Filters: FiltersScreen
+}, {
+  // navigationOptions: {
+  //   drawerLabel: 'Filters!!!'
+  // },
+  defaultNavigationOptions: defaultStackNavOptions
 });
 
 const MainNavigator = createDrawerNavigator({
-  MealsFavs: MealsFavTabNavigator,
+  MealsFavs: { 
+    screen: MealsFavTabNavigator,
+    navigationOptions: {
+      drawerLabel: 'Meals'
+    }
+  },
   Filters: FiltersNavigator
+}, {
+  contentOptions: {
+    activeTintColor: Colors.accentColor,
+    labelStyle: {
+      fontFamily: 'open-sans-bold'
+    }
+  }
 });
 
 export default createAppContainer(MainNavigator);
