c96f05cf78be1fa148580f8b8cd428cd8210fe7a
Michel Pereira <michel.augusto@pg.inatel.br>
2019-09-23 21:41:30 -0300

    finish app meals

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { Item, HeaderButtons } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton';
 import MealList from '../components/MealList';
+import DefaltText from '../components/DefaultText';
 
 const FavoritesScreen = props => {
   const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    return (
+      <View style={styles.content}>
+        <DefaltText>No favorite meals Found. Start adding some!</DefaltText>
+      </View>
+    )
+  }
+
   return <MealList listData={favMeals} navigation={props.navigation} />
 };
 
@@ -22,5 +33,13 @@ FavoritesScreen.navigationOptions = (navData) => {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
 
 export default FavoritesScreen;
--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, StyleSheet, Switch, Platform } from 'react-native';
 import { Item, HeaderButtons } from 'react-navigation-header-buttons';
+import { useDispatch } from 'react-redux';
 
 import HeaderButton from '../components/HeaderButton';
 import Colors from '../constants/Colors';
+import { setFilters } from '../store/actions/meals';
 
 const FilterSwitch = props => {
   return (
@@ -27,6 +29,8 @@ const FiltersScreen = props => {
   const [isVegan, setIsVegan] = useState(false);
   const [isVegetarian, setIsVegetarian] = useState(false);
 
+  const dispatch = useDispatch();
+
   const saveFilters = useCallback(() => {
     const appliedFilters = {
       glutenFree: isGlutenFree,
@@ -35,7 +39,7 @@ const FiltersScreen = props => {
       isVegetarian: isVegetarian
     }
 
-    console.log(appliedFilters);
+    dispatch(setFilters(appliedFilters));
   }, [isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
 
   useEffect(() => {
