5d23161b6f87b5038119364d5751da9e185b88f1
Michel Pereira <michel.augusto@pg.inatel.br>
2019-09-24 21:42:34 -0300

    add navigation

--- store/reducers/products.js
+++ store/reducers/products.js
@@ -1,6 +1,8 @@
+import PRODUCTS from '../../data/dummy-data';
+
 const initialState = {
-  avaliableProducts: [],
-  userProducts: []
+  availableProducts: PRODUCTS,
+  userProducts: PRODUCTS.filter(prod => prod.ownerId === 'u1')
 };
 
 export default (state = initialState, action) => {
