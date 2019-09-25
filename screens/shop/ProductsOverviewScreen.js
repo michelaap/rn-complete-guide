bc6e46c59babde83f6b392c738074da0437e1068
Michel Pereira <michel.augusto@pg.inatel.br>
2019-09-24 21:04:22 -0300

    first commit

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+
+const ProductsOverviewScreen = props => {
+  return (
+    <FlatList />
+  );
+};
+
+export default ProductsOverviewScreen;
---
+++ store/reducers/products.js
@@ -0,0 +1,8 @@
+const initialState = {
+  avaliableProducts: [],
+  userProducts: []
+};
+
+export default (state = initialState, action) => {
+  return state;
+};
