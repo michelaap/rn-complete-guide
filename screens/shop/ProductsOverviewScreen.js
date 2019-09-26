80c4665a5ec21d050a04a48eb1b26f363744f143
Michel Pereira <michel.augusto@pg.inatel.br>
2019-09-26 20:54:20 -0300

    add items to the cart

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { FlatList, Text } from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import ProductItem from '../../components/shop/ProductItem';
+import * as cartAction from '../../store/actions/cart';
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
+  const dispatch = useDispatch();
+  
   return (
     <FlatList
       data={products}
@@ -27,7 +30,9 @@ const ProductsOverviewScreen = props => {
               }
             })
           }}
-          onAddToCart={() => {}}
+          onAddToCart={() => {
+            dispatch(cartAction.addToCart(itemData.item));
+          }}
         />
       }
     />
