3f7330f62f11cd8572cc22279e56134fa1785cb8
Michel Pereira <michel.augusto@pg.inatel.br>
2019-10-01 21:54:44 -0300

    add order item

--- models/order.js
+++ models/order.js
@@ -5,6 +5,16 @@ class Order {
     this.totalAmount = totalAmount,
     this.date = date
   }
+
+  get readableDate() {
+    return this.date.toLocaleDateString('pt-BR', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    });
+  }
 }
 
 export default Order;
