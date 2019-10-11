6909533ddb299c1e8cc976f29e310d38502fccb5
Michel Pereira <michel.augusto@pg.inatel.br>
2019-10-11 20:27:24 -0300

    add card component

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -9,6 +9,8 @@ import {
   TouchableNativeFeedback
 } from 'react-native';
 
+import Card from '../UI/Card';
+
 const ProductItem = props => {
   let TouchableComponent = TouchableOpacity;
 
@@ -17,7 +19,7 @@ const ProductItem = props => {
   }
 
   return (
-    <View style={styles.product}>
+    <Card style={styles.product}>
       <View style={styles.touchable}>
         <TouchableComponent onPress={props.onSelect} useForegroud>
           <View>
@@ -34,19 +36,12 @@ const ProductItem = props => {
           </View>
         </TouchableComponent>
       </View>
-    </View>
+    </Card>
   );
 };
 
 const styles = StyleSheet.create({
   product: {
-    shadowColor: 'black',
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: 'white',
     height: 300,
     margin: 20
   },
--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
+import Card from '../../components/UI/Card';
 import * as cartActions from '../../store/actions/cart';
 import * as ordersActions from '../../store/actions/order';
 
@@ -30,7 +31,7 @@ const CartScreen = props => {
 
   return (
     <View style={styles.screen}>
-      <View style={styles.summary}>
+      <Card style={styles.summary}>
         <Text style={styles.summaryText}>
           Total: <Text style={styles.amount}>${Math.round(cartTotalAmount.toFixed(2) * 100) / 100}</Text>
         </Text>
@@ -42,7 +43,7 @@ const CartScreen = props => {
             dispatch(ordersActions.addOrder(cartItems, cartTotalAmount))
           }}
         />
-      </View>
+      </Card>
       <FlatList 
         data={cartItems}
         keyExtractor={item => item.productId}
@@ -72,13 +73,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     marginBottom: 20,
     padding: 10,
-    shadowColor: 'black',
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: 'white',
   },
   summaryText: {
     fontFamily: 'open-sans-bold',
