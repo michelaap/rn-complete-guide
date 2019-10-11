5f38995bf15380bc5e79239fe03ebd69dd217f39
Michel Pereira <michel.augusto@pg.inatel.br>
2019-10-10 21:43:43 -0300

    dispating actions

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { View, ScrollView, Text, StyleSheet, TextInput, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import HeaderButton from '../../components/UI/HeaderButton';
+import * as productActions from '../../store/actions/products';
 
 const EditProductScreen = props => {
   const prodId = props.navigation.getParam('productId');
@@ -11,18 +12,24 @@ const EditProductScreen = props => {
     state => state.products.userProducts.find(prod => prod.id === prodId)
   );
 
-  const [title, setTitle] = useState(editedProduct.title || '');
-  const [imageUrl, setImageUrl] = useState(editedProduct.imageUrl || '');
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState(editedProduct ? editedProduct.title : '');
+  const [imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '');
   const [price, setPrice] = useState('');
-  const [description, setDescription] = useState(editedProduct.description || '');
+  const [description, setDescription] = useState(editedProduct ? editedProduct.description : '');
 
   const submitHandler = useCallback(() => {
-    console.log('Submiting')
-  });
+    if (editedProduct) {
+      dispatch(productActions.updateProduct(prodId, title, description, imageUrl))
+    } else {
+      dispatch(productActions.createProduct(title, description, imageUrl, +price))
+    }
+  }, [dispatch, prodId, title, description, imageUrl, price]);
 
   useEffect(() => {
     props.navigation.setParams({ submit: submitHandler })
-  }, [])
+  }, [submitHandler])
 
   return (
     <ScrollView>
